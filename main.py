6614a9003b12cfdc42ceab3de1768fff58bf9991
SKPG <42890752+SKPG-GitHub@users.noreply.github.com>
2020-08-26 23:24:56 +0300

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 serial.redirect_to_usb()
 serial.set_baud_rate(BaudRate.BAUD_RATE115200)
-command = "null"
-alive = True
-enter = True
 serial.write_line("Welcome to the first micro:bit shell.")
-while alive:
-    serial.write_string("" + control.device_name() + " ~ % ")
-    while enter:
-        serial.write_string(serial.read_string())
+serial.write_string("" + control.device_name() + " ~ % ")
+
+def on_forever():
+    pass
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,9 +1,41 @@
+serial.onDataReceived(serial.delimiters(Delimiters.SemiColon), function () {
+    pause(100)
+if (command == "help") {
+        serial.writeString("" + serial.NEW_LINE + "List of commands:")
+    } else if (command == "") {
+    	
+    } else if (command == "") {
+    	
+    } else {
+        serial.writeString("" + serial.NEW_LINE + "Error: command '" + command + "' not found!")
+    }
+    command = ""
+    new_command()
+})
+function new_command () {
+    if (alive) {
+        serial.writeString("" + serial.NEW_LINE + "" + control.deviceName() + " ~ % ")
+    }
+}
+let letter = ""
+let command = ""
+let alive = false
 serial.redirectToUSB()
 serial.setBaudRate(BaudRate.BaudRate115200)
-let command = ""
-serial.writeLine("Welcome to the first micro:bit shell.")
-serial.writeString("" + control.deviceName() + " ~ % ")
-serial.writeString(serial.readString())
+serial.writeString("Welcome to the first micro:bit shell.")
+alive = true
+new_command()
 basic.forever(function () {
-    
+    if (alive) {
+        letter = serial.readString()
+        serial.writeString(letter)
+        if (letter == ";") {
+            letter = ""
+        } else {
+            command = "" + command + letter
+            letter = ""
+        }
+    } else {
+    	
+    }
 })
