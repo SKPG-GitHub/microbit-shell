5b087dbd1f1be496b76f810306ff66d9c0eecc86
SKPG <42890752+SKPG-GitHub@users.noreply.github.com>
2020-08-27 12:11:59 +0300

    Update main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,22 +1,26 @@
-serial.onDataReceived(serial.delimiters(Delimiters.SemiColon), function () {
+serial.onDataReceived(serial.delimiters(Delimiters.SemiColon), function on_data_received() {
+    
     pause(100)
-if (command == "help") {
+    if (command == "help") {
         serial.writeString("" + serial.NEW_LINE + "List of commands:")
     } else if (command == "") {
-    	
+        
     } else if (command == "") {
-    	
+        
     } else {
         serial.writeString("" + serial.NEW_LINE + "Error: command '" + command + "' not found!")
     }
+    
     command = ""
     new_command()
 })
-function new_command () {
+function new_command() {
     if (alive) {
         serial.writeString("" + serial.NEW_LINE + "" + control.deviceName() + " ~ % ")
     }
+    
 }
+
 let letter = ""
 let command = ""
 let alive = false
@@ -25,7 +29,8 @@ serial.setBaudRate(BaudRate.BaudRate115200)
 serial.writeString("Welcome to the first micro:bit shell.")
 alive = true
 new_command()
-basic.forever(function () {
+basic.forever(function on_forever() {
+    
     if (alive) {
         letter = serial.readString()
         serial.writeString(letter)
@@ -35,7 +40,6 @@ basic.forever(function () {
             command = "" + command + letter
             letter = ""
         }
-    } else {
-    	
+        
     }
 })
