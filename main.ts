6d20c6d1cadf4c1b233c7d9ee3ad1d9b16054301
Dominykas M <26067386+PapuasinisPingvinas@users.noreply.github.com>
2020-08-27 16:02:13 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,37 +1,30 @@
 serial.onDataReceived(serial.delimiters(Delimiters.SemiColon), function () {
     pause(100)
-    command = command.toLowerCase()
-    if (command == "help") {
-          serial.writeString("" + "\r\nList of commands:" + 
-            "\r\nhelp - shows a list of commands" + 
-            "\r\ninfo - shows info about this device")
-        } else if (command == "info") {
-            serial.writeString("\r\nInfo about this device:" + 
-            "\r\nDevice Name: " + control.deviceName() + 
-            "\r\nDevice Serial Number: " + control.deviceSerialNumber() + 
-            "\r\nTime Elapsed Since Boot: " + (control.millis() / 1000) + " s")
-        } else if (command == "about") {
-            serial.writeString("\r\nThis is a shell, made using the Makecode editor with Javascript.\r\nAnd the thing is it runs on a micro:bit")
-        } else if (command == "reset") {
-            serial.writeString("\r\nTHE MICRO:BIT WILL RESET DON'T TOUCH ANYTHING\r\n")
-            control.reset()
-        } else {
-            serial.writeString("\r\nError: command '" + command + "' not found!")
-        }
+command = command.toLowerCase()
+if (command == "help") {
+        serial.writeString("" + "\r\nList of commands:" + "\r\nhelp - shows a list of commands" + "\r\ninfo - shows info about this device")
+    } else if (command == "info") {
+        serial.writeString("" + "\r\nInfo about this device:" + "\r\nDevice Name: " + control.deviceName() + "\r\nDevice Serial Number: " + control.deviceSerialNumber() + "\r\nTime Elapsed Since Boot: " + control.millis() / 1000 + " s")
+    } else if (command == "about") {
+        serial.writeString("" + ("\r\nThis is a shell, made using the Makecode editor with Javascript.\r\nAnd the thing is it runs on a micro:bit"))
+    } else if (command == "reset") {
+        serial.writeString("" + ("\r\nTHE MICRO:BIT WILL RESET DON'T TOUCH ANYTHING\r\n"))
+        control.reset()
+    } else {
+        serial.writeString("" + "\r\nError: command '" + command + "' not found!")
+    }
     command = ""
     new_command()
 })
-
 function new_command () {
-    serial.writeString("\r\n" + control.deviceName() + " ~ % ")
+    serial.writeString("" + "\r\n" + control.deviceName() + " ~ % ")
 }
-
 let letter = ""
 let command = ""
 serial.redirectToUSB()
 serial.setBaudRate(BaudRate.BaudRate115200)
 serial.writeString("Welcome to the first micro:bit shell.")
-serial.writeString("\r\nPress ';' to run the command. (Enter doesn't work)")
+serial.writeString("" + ("\r\nPress ';' to run the command. (Enter doesn't work)"))
 new_command()
 basic.forever(function () {
     letter = serial.readString()
