8529cc06543adf59b6fc10208ce9397f09f0be16
SKPG <42890752+SKPG-GitHub@users.noreply.github.com>
2020-08-27 15:31:12 +0300

    Update pxt.json, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,42 +1,48 @@
-serial.onDataReceived(serial.delimiters(Delimiters.SemiColon), function () {
+serial.onDataReceived(serial.delimiters(Delimiters.SemiColon), function on_data_received() {
+    
     pause(100)
-    command = command.toLowerCase()
     if (command == "help") {
-          serial.writeString("" + "\r\nList of commands:" + 
-            "\r\nhelp - shows a list of commands" + 
-            "\r\ninfo - shows info about this device")
-        } else if (command == "info") {
-            serial.writeString("\r\nInfo about this device:" + 
-            "\r\nDevice Name: " + control.deviceName() + 
-            "\r\nDevice Serial Number: " + control.deviceSerialNumber() + 
-            "\r\nTime Elapsed Since Boot: " + (control.millis() / 1000) + " s")
-        } else if (command == "about") {
-            serial.writeString("This is a shell, made using the Makecode editor with Javascript and the thing is it runs on a micro:bit")
-        } else {
-            serial.writeString("" + serial.NEW_LINE + "Error: command '" + command + "' not found!")
-        }
+        serial.writeString("" + serial.NEW_LINE + "List of commands:")
+    } else if (command == "") {
+        
+    } else if (command == "") {
+        
+    } else {
+        serial.writeString("" + serial.NEW_LINE + "Error: command '" + command + "' not found!")
+    }
+    
     command = ""
     new_command()
 })
-
-function new_command () {
-    serial.writeString("\r\n" + control.deviceName() + " ~ % ")
+function new_command() {
+    if (alive) {
+        serial.writeString("" + serial.NEW_LINE + "" + control.deviceName() + " ~ % ")
+    }
+    
 }
 
 let letter = ""
 let command = ""
+let alive = false
 serial.redirectToUSB()
 serial.setBaudRate(BaudRate.BaudRate115200)
 serial.writeString("Welcome to the first micro:bit shell.")
-serial.writeString("\r\nPress ';' to run the command. (Enter doesn't work)")
+alive = true
 new_command()
-basic.forever(function () {
-    letter = serial.readString()
-    serial.writeString(letter)
-    if (letter == ";") {
-        letter = ""
+basic.forever(function on_forever() {
+    
+    if (alive) {
+        letter = serial.readString()
+        serial.writeString(letter)
+        if (letter == ";") {
+            letter = ""
+        } else {
+            command = "" + command + letter
+            letter = ""
+        }
+        
     } else {
-        command = "" + command + letter
-        letter = ""
+        
     }
+    
 })
