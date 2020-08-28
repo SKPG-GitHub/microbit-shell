73f936e4bc14e7cb6e7db85881a88e713e4b4ab1
Dominykas M <26067386+PapuasinisPingvinas@users.noreply.github.com>
2020-08-28 12:23:45 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,18 @@
 serial.onDataReceived(serial.delimiters(Delimiters.SemiColon), function () {
     pause(100)
-    command = command.toLowerCase()
-    if (command == "help") {
-        serial.writeString("" + "\r\nList of commands:" + 
-        "\r\nhelp - shows a list of commands" + 
-        "\r\ninfo - shows info about this device" + 
-        "\r\nabout - shows info about the flashed program on this device" + 
-        "\r\nreset - restarts the device" + 
-        "\r\n")
+command = command.toLowerCase()
+if (command == "help") {
+        serial.writeString("" + "\r\nList of commands:" + "\r\nhelp - shows a list of commands" + "\r\ninfo - shows info about this device" + "\r\nabout - shows info about the flashed program on this device" + "\r\nreset - restarts the device" + "\r\n")
     } else if (command == "info") {
-        serial.writeString("" + "\r\nInfo about this device:" + 
-        "\r\nDevice Name: " + control.deviceName() + 
-        "\r\nDevice Serial Number: " + control.deviceSerialNumber() + 
-        "\r\nTime Elapsed Since Boot: " + control.millis() / 1000 + " s" + 
-        "\r\nShell Version: v1.0.1-alpha" + 
-        "\r\nDevice Temperature: " + input.temperature() + "C, " + ((input.temperature() * 1.8) + 32) + "F")
+        serial.writeString("" + "\r\nInfo about this device:" + "\r\nDevice Name: " + control.deviceName() + "\r\nDevice Serial Number: " + control.deviceSerialNumber() + "\r\nTime Elapsed Since Boot: " + control.millis() / 1000 + " s" + "\r\nShell Version: v1.0.1-alpha" + "\r\nDevice Temperature: " + input.temperature() + "C, " + (input.temperature() * 1.8 + 32) + "F")
     } else if (command == "about") {
-        serial.writeString("" + "\r\nThis is a shell, made using the Makecode editor with Javascript." + 
-        "\r\nAnd the thing is it runs on a micro:bit.")
+        serial.writeString("" + "\r\nThis is a shell, made using the Makecode editor with Javascript." + "\r\nAnd the thing is it runs on a micro:bit.")
     } else if (command == "reset") {
         serial.writeString("" + ("\r\nTHE MICRO:BIT WILL RESET DON'T TOUCH ANYTHING\r\n"))
         control.reset()
     } else if (command.includes("pinread ")) {
         let neww: string = command.replace("pinread ", "")
-        switch (neww) {
+switch (neww) {
             case "0":
                 serial.writeString("\r\nVoltage on PIN0: " + pins.analogReadPin(AnalogPin.P0))
                 break
@@ -69,14 +58,13 @@ serial.onDataReceived(serial.delimiters(Delimiters.SemiColon), function () {
                 serial.writeString("\r\nError: wrong pin number/index!")
         }
     } else if (command == "") {
-            
+    	
     } else {
         serial.writeString("" + "\r\nError: command '" + command + "' not found!")
     }
     command = ""
     new_command()
 })
-
 function new_command () {
     if (num) {
         serial.writeString("" + "\r\n" + control.deviceName() + " ~ % ")
@@ -84,10 +72,9 @@ function new_command () {
         serial.writeString("" + control.deviceName() + " ~ % ")
     }
 }
-
 let letter = ""
-let command = ""
 let num = false
+let command = ""
 serial.redirectToUSB()
 serial.setBaudRate(BaudRate.BaudRate115200)
 new_command()
